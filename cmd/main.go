f696628faeeb8a159ad8b42725fb32308f028fc8
LLlE0 <glider.coolhacker@gmail.com>
2023-12-22 19:58:08 +0300

    Avoiding overengineering
    Removed extra module, .exe program opens  browser

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/D-building-anonymaizer/backend-service"
 	"github.com/D-building-anonymaizer/backend-service/pkg/handler"
-	"github.com/D-building-anonymaizer/backend-service/pkg/repository"
 	"github.com/D-building-anonymaizer/backend-service/pkg/service"
 	"github.com/spf13/viper"
 	"log"
@@ -13,10 +12,11 @@ func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("Error while initializing the config file: %s", err.Error())
 	}
-	repos := repository.NewRepository()
-	services := service.NewService(repos)
+
+	services := service.NewService(viper.GetString("ip"), viper.GetString("port"))
 	handlers := handler.NewHandler(services)
 	srv := new(backend.Server)
+	services.RunApp()
 	if err := srv.Run(viper.GetString("port"), viper.GetString("ip"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error while running http server: %s", err.Error())
 	}
--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,10 +1,40 @@
 package service
 
-import "github.com/D-building-anonymaizer/backend-service/pkg/repository"
+import (
+	"errors"
+	"log"
+	"os/exec"
+	"runtime"
+)
 
 type Service struct {
+	url string
 }
 
-func NewService(repos *repository.Repository) *Service {
-	return &Service{}
+func (s *Service) RunApp() error {
+	var cmd string
+
+	ops := runtime.GOOS
+	switch ops {
+	case "windows":
+		cmd = "start "
+	case "darwin":
+		cmd = "open "
+	case "linux":
+		cmd = "xdg-open "
+	default:
+		return errors.New("error in OS switch")
+	}
+
+	log.Print(cmd, s.url)
+	c := exec.Command("cmd", "/C", cmd, s.url)
+	err := c.Start()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewService(ipAddr, port string) *Service {
+	return &Service{"http://" + ipAddr + ":" + port + "/"}
 }
